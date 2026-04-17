38e367926036bbe89602941a5347d127542a5e69
santiagoFassio <fassiosantiago@gmail.com>
2026-04-17 15:24:25 -0300

    Feat: Middleware Queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,49 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.host = host
+        self.queue_name = queue_name
+
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host)
+        )
+
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
+    
+    def send(self, message: bytes):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.queue_name,
+            body=message
+        )
+    
+    def start_consuming(self, callback):
+        def on_message(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+
+            def nack():
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+
+            callback(body, ack, nack)
+        
+        self.channel.basic_consume(
+            queue=self.queue_name,
+            on_message_callback=on_message,
+            auto_ack=False
+        )
+
+        self.channel.start_consuming()
+
+    def close(self):
+        if self.connection and self.connection.is_open:
+            self.connection.close()
+
+    def stop_consuming(self):
+        if self.channel:
+            self.channel.stop_consuming()
+
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
