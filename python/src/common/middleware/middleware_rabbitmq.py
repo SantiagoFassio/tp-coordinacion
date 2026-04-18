0964eadc3a6a83592b6bd0c08de2de40c50fe0e0
santiagoFassio <fassiosantiago@gmail.com>
2026-04-18 13:37:13 -0300

    Feat: Implementation of rabbit exchnge Passes 1st step

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -20,7 +20,8 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         self.channel.basic_publish(
             exchange='',
             routing_key=self.queue_name,
-            body=message
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2)
         )
     
     def start_consuming(self, callback):
@@ -46,11 +47,69 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self.connection.close()
 
     def stop_consuming(self):
-        if self.channel:
+        if self.channel and self.channel.is_open:
             self.channel.stop_consuming()
 
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.host = host
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host)
+        )
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(
+            exchange=self.exchange_name,
+            exchange_type='direct',
+            durable=True
+        )
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.queue_name = result.method.queue
+
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(
+                exchange=self.exchange_name,
+                queue=self.queue_name,
+                routing_key=routing_key
+            )
+    
+    def send(self, message: bytes):
+        for routing_key in self.routing_keys:
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=routing_key,
+                body=message,
+                properties=pika.BasicProperties(delivery_mode=2)
+            )
+        
+    def start_consuming(self, callback):
+        def on_message(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+
+            def nack():
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+
+            callback(body, ack, nack)
+        
+        self.channel.basic_consume(
+            queue=self.queue_name,
+            on_message_callback=on_message,
+            auto_ack=False
+        )
+
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        if self.channel and self.channel.is_open:
+            self.channel.stop_consuming()
+    
+    def close(self):
+        if self.connection and self.connection.is_open:
+            self.connection.close()
